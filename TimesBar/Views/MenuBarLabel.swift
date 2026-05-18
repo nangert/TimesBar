dcdef548e712dda10e14a59e7d131566037b545a
nangert <n.angert@lipso.dev>
2026-05-18 23:50:08 +0900

    feat: render compact menu bar label with dot, elapsed, sparkline

--- TimesBar/Views/MenuBarLabel.swift
+++ TimesBar/Views/MenuBarLabel.swift
@@ -2,7 +2,18 @@ import SwiftUI
 
 struct MenuBarLabel: View {
     @EnvironmentObject var store: TimerStore
+
     var body: some View {
-        Image(systemName: store.isRunning ? "timer" : "timer")
+        HStack(spacing: 6) {
+            Circle()
+                .fill(store.isRunning ? Color.green : Color.secondary)
+                .frame(width: 7, height: 7)
+            if store.isRunning {
+                Text(store.elapsedString)
+                    .font(.system(.body, design: .monospaced))
+            }
+            Sparkline(values: store.weekHours)
+                .frame(width: 48, height: 14)
+        }
     }
 }
